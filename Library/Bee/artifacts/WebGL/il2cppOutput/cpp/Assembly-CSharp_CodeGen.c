dafd4026fe12c52a362fd82179e4a0a79b45edb9
Stephanie7272 <stephanie.pileggi@knights.ucf.edu>
2022-10-20 17:52:12 -0400

    First Commit

---
+++ Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,73 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void CameraScript::Start()
+extern void CameraScript_Start_mA5B7202B365DD49486C67738FBD1EC84D104BE33 (void);
+// 0x00000002 System.Void CameraScript::LateUpdate()
+extern void CameraScript_LateUpdate_m42F00D6F15A466E628762DC7ADBD8272EF43F583 (void);
+// 0x00000003 System.Void CameraScript::.ctor()
+extern void CameraScript__ctor_mDCE666CE7EF072645A59FD192F310106688922B2 (void);
+// 0x00000004 System.Void PlayerScripts::Start()
+extern void PlayerScripts_Start_mBB2D6BDC9C21729DBBA62C1BE6B33A6E829825D4 (void);
+// 0x00000005 System.Void PlayerScripts::FixedUpdate()
+extern void PlayerScripts_FixedUpdate_mE94F7EB840FDAEB71BAEF4AE85314CC4672CC43F (void);
+// 0x00000006 System.Void PlayerScripts::OnCollisionEnter2D(UnityEngine.Collision2D)
+extern void PlayerScripts_OnCollisionEnter2D_m248C1F0E7C2AB0F8880D6F5268C3F4ED2ABFC896 (void);
+// 0x00000007 System.Void PlayerScripts::OnCollisionStay2D(UnityEngine.Collision2D)
+extern void PlayerScripts_OnCollisionStay2D_m26E48F9239B3F7DF376FE5A27A6B73CFDC06A877 (void);
+// 0x00000008 System.Void PlayerScripts::.ctor()
+extern void PlayerScripts__ctor_mDE09F686B92001D82B8C43E78804B8E78B6B68E2 (void);
+static Il2CppMethodPointer s_methodPointers[8] = 
+{
+	CameraScript_Start_mA5B7202B365DD49486C67738FBD1EC84D104BE33,
+	CameraScript_LateUpdate_m42F00D6F15A466E628762DC7ADBD8272EF43F583,
+	CameraScript__ctor_mDCE666CE7EF072645A59FD192F310106688922B2,
+	PlayerScripts_Start_mBB2D6BDC9C21729DBBA62C1BE6B33A6E829825D4,
+	PlayerScripts_FixedUpdate_mE94F7EB840FDAEB71BAEF4AE85314CC4672CC43F,
+	PlayerScripts_OnCollisionEnter2D_m248C1F0E7C2AB0F8880D6F5268C3F4ED2ABFC896,
+	PlayerScripts_OnCollisionStay2D_m26E48F9239B3F7DF376FE5A27A6B73CFDC06A877,
+	PlayerScripts__ctor_mDE09F686B92001D82B8C43E78804B8E78B6B68E2,
+};
+static const int32_t s_InvokerIndices[8] = 
+{
+	3197,
+	3197,
+	3197,
+	3197,
+	3197,
+	2612,
+	2612,
+	3197,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	8,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
